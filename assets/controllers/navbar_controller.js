8061831297d44f133fae0492c09beda163db19cb
maxc0434 <max.couillet@gmail.com>
2026-06-03 16:16:49 +0200

    loginpage style ok

--- assets/controllers/navbar_controller.js
+++ assets/controllers/navbar_controller.js
@@ -2,27 +2,18 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
+        
         this.dropdownElements = this.element.querySelectorAll('.dropdown');
 
         this.dropdownElements.forEach(dropdown => {
-            const toggle = dropdown.querySelector('.dropdown-toggle');
             const menu = dropdown.querySelector('.dropdown-menu');
 
             dropdown.addEventListener('show.bs.dropdown', () => {
-                menu.classList.add('show');
+                if (menu) menu.classList.add('animate-fade-in');
             });
 
-            dropdown.addEventListener('hide.bs.dropdown', (e) => {
-                e.preventDefault();
-                menu.classList.remove('show');
-                
-                setTimeout(() => {
-                    const instance = bootstrap.Dropdown.getInstance(toggle);
-                    if (instance) {
-                        dropdown.classList.remove('show');
-                        toggle.setAttribute('aria-expanded', 'false');
-                    }
-                }, 250);
+            dropdown.addEventListener('hide.bs.dropdown', () => {
+                if (menu) menu.classList.remove('animate-fade-in');
             });
         });
     }
